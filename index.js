c72e005c0ae4e2ccb225becfa0e7f623ee12dbb3
heavy-matill <heavymatill@gmail.com>
2024-03-03 22:18:45 +0100

    merged into index.js

--- index.js
+++ index.js
@@ -188,3 +188,31 @@ parseUG();
     console.log(request.greeting);
     return Promise.resolve({ response: "Hi from content script" });
   });*/
+
+chrome.runtime.onMessage.addListener(
+        function (request, sender, sendResponse) {
+                console.log(request);
+                elMeta.innerHTML = ""
+                for (key in request) {
+                        if (key != "chordSheet") {
+                                let elDiv = document.createElement("div")
+                                let elChk = document.createElement("input")
+                                elChk.type = "checkbox"
+                                elChk.addEventListener('change', parseUG)
+                                console.log(key)
+                                elChk.checked = !key.startsWith("comment: ")
+                                let elLab = document.createElement("label")
+                                elLab.innerText = key
+                                let elVal = document.createElement("input")
+                                elVal.type = "text"
+                                elVal.value = request[key]
+                                elVal.addEventListener('input', parseUG)
+                                for (el of [elChk, elLab, elVal])
+                                        elDiv.appendChild(el)
+                                elMeta.appendChild(elDiv)
+                        }
+                }
+                elSrc.value = request.chordSheet ?? ""
+                parseUG();
+        }
+);
